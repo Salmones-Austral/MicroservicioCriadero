4aac7da8131edd1c6ec2949d9200b6788e655074
JorgePMC <formalidad.jpg@gmail.com>
2026-05-22 11:48:42 -0400

    subir aaaaaaaaaaaaaa

--- src/main/java/cl/SalmonesAustral/Criadero/model/Criadero.java
+++ src/main/java/cl/SalmonesAustral/Criadero/model/Criadero.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-
 //Entidad JPA para representar un Criadero (Centro de Cultivo)
 
 @Entity
